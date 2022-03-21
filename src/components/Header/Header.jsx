7251388e69b43515aaae3b0a3032fb28988636ec
Uzair Ahmed <uzairahmed74372@gmail.com>
2022-03-21 23:55:33 +0500

    added motion effect on header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from 'framer-motion';
 import { useState, useEffect } from 'react/cjs/react.development';
 import Search from '../Search/Search';
 
@@ -8,21 +9,28 @@ const Header = () => {
   }, [isTyping]);
 
   return (
-    <>
-      {!isTyping && (
-        <div className="text-center text-white  w-full md:w-[80%]">
-          <h1 className="text-5xl sm:text-8xl font-roboto font-bold">
-            GitHub Finder
-          </h1>
-          <p className="mb-12 mt-4 text-base sm:text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
-            illo, aliquid quos laudantium sit exercitationem accusantium natus
-            corporis repellat asperiores nulla minus assumenda totam impedit.
-          </p>
-        </div>
-      )}
-      <Search isTyping={setIsTyping} />
-    </>
+    <AnimatePresence>
+      <motion.div
+        className="flex flex-col items-center"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      >
+        {!isTyping && (
+          <div className="text-center text-white  w-full md:w-[80%]">
+            <h1 className="text-5xl sm:text-8xl font-roboto font-bold">
+              GitHub Finder
+            </h1>
+            <p className="mb-12 mt-4 text-base sm:text-xl">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+              illo, aliquid quos laudantium sit exercitationem accusantium natus
+              corporis repellat asperiores nulla minus assumenda totam impedit.
+            </p>
+          </div>
+        )}
+        <Search isTyping={setIsTyping} />
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
